e73a7c8a8c13d1ab44329015bc48e805874a76a0
Thomas Schillaci <thomas.schillaci@gmail.com>
2019-03-16 17:19:24 +0100

    Restructure

---
+++ VieML/ML/main.py
@@ -0,0 +1,26 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import math
+import matplotlib.pyplot as plot
+
+
+def test():
+    model = Sequential()
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+    n = 1000
+    x = np.array([[k / n * math.pi * 2] for k in range(n)], "float32")
+    y = np.array([[(math.sin(k / n * math.pi * 2)+1)/2] for k in range(n)], "float32")
+
+    model.fit(x, y, epochs=1000)
+
+    y_predict = model.predict(x)
+
+    plot.plot(x, y, x, y_predict)
+    plot.show()
+
+
+test()
