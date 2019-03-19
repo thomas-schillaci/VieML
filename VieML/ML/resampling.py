6e2c698805a7ed6482ff389e5fda9876de7f0f14
Somerlinck <leo.vuylsteker@imt-atlantique.net>
2019-03-19 23:35:44 +0100

    fonction de resampling d'image Léo

---
+++ VieML/ML/resampling.py
@@ -0,0 +1,25 @@
+from scipy import ndimage, misc
+import matplotlib.pyplot as plt
+
+test_image = misc.ascent()
+
+def resample(image,zoom,i):
+    '''
+    Resamples a given image
+
+    :param image:
+    :param zoom: size of the resampled image compared with the original
+    :param i: Order of the spline interpolation
+                0 : 2D-nearest neighbors
+                1 : Bilinear
+                2 : Bicubic
+    :return: A resampled image
+    '''
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)  # left side
+    ax2 = fig.add_subplot(122)  # right side
+    result = ndimage.zoom(image, zoom=zoom, order = i)
+    ax1.imshow(image)
+    ax2.imshow(result)
+    plt.show()
+    return result
