40cc38aca716d30500622b6ee23d8519316ba4b8
FLeGuernic <francois.leguernic@gmail.com>
2019-05-28 20:55:51 +0200

    pb noms de fichiers réglé

--- VieML/IHM/InterfaceOCV.py
+++ VieML/IHM/InterfaceOCV.py
@@ -1,13 +1,17 @@
+
 from PySide2 import QtCore, QtWidgets, QtGui
 import sys
 import numpy as np
 import cv2
 import argparse
-
-from PySide2.QtGui import QPixmap
-from PySide2.QtWidgets import QLabel
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
 import os
-
+from math import *
 class Fenetre(QtWidgets.QDialog):
 
    def __init__(self):
@@ -21,6 +25,8 @@ class Fenetre(QtWidgets.QDialog):
        self.button3 = QtWidgets.QPushButton("save")
        self.button4 = QtWidgets.QPushButton(QtGui.QIcon("index.png"),"Dataset preparation (video)")
        self.button5=QtWidgets.QPushButton("afficher")
+       self.Compteur=QtWidgets.QLabel("")
+       self.Compteur.setAlignment(Qt.AlignCenter)
        # Create layout and add widget
        self.button4.clicked.connect(self.dataSetVideo)
        self.button5.clicked.connect(self.afficher)
@@ -33,6 +39,7 @@ class Fenetre(QtWidgets.QDialog):
        label = QLabel()
        pixmap = QPixmap('IA.jpg')
        label.setPixmap(pixmap)
+       label.setAlignment(Qt.AlignCenter)
 
        #layout.addWidget(self.button5)
        layout.addWidget(label)
@@ -40,7 +47,7 @@ class Fenetre(QtWidgets.QDialog):
        layout.addWidget(self.button2)
        #layout.addWidget(self.button3)
        layout.addWidget(self.button4)
-       layout.maximumSize()
+       layout.addWidget(self.Compteur)
 
        # Set  layout
        self.setLayout(layout)
@@ -115,22 +122,40 @@ class Fenetre(QtWidgets.QDialog):
                j = j + 1
            print(Dossier)
            frame_height, frame_width = (self.ListeFrame[0]).shape[:2]
-           out = cv2.VideoWriter(Dossier+'/VieML '+str(i)+".avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 8,(frame_width, frame_height))  # 2eme parametre=fps de la video
+           out = cv2.VideoWriter(Dossier+'/VieML '+str(i)+".avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20,(frame_width, frame_height))  # 2eme parametre=fps de la video
            for i in range(len(self.ListeFrame)):
                out.write(self.ListeFrame[i])
            out.release()
        cv2.destroyAllWindows()
 
    def dataSet(self):
-       Dossier = QtWidgets.QFileDialog.getExistingDirectoryUrl(caption="SELECTIONNER DOSSIER DE DESTINATIION").path()[1:]
+       Dossier = QtWidgets.QFileDialog.getExistingDirectoryUrl(caption="Selectionner dossier de destination").path()[1:]
        fileName, _ = QtWidgets.QFileDialog.getOpenFileNames(self, caption="open file")
        self.adresse=fileName
        AncienFichier=os.listdir(Dossier)
+       print(AncienFichier)
        if(AncienFichier==[]):
            DerniereUtilisation=0
        else:
-           DerniereUtilisation=int(AncienFichier[-1][0])
+           index=0
+           DerniereUtilisation=1
+           test=True
+           eliminerzeros=True
+           while(test):
+               try:
+                   if(eliminerzeros):
+                       if AncienFichier[-1][index]!="0":
+                           DerniereUtilisation = DerniereUtilisation + int(AncienFichier[-1][index]) * int(pow(10, index))
+                           eliminerzeros=False
+                       index=index+1
+                   else:
+                       DerniereUtilisation=DerniereUtilisation+int(AncienFichier[-1][index])*int(pow(10,2-index))
+                       index = index + 1
+               except ValueError or OverflowError:
+                   test=False
+                   break
        for i in range(len(fileName)):
+           self.Compteur.setText(str(i)+"/"+str(len(fileName)))
            self.ListeFrame = []
            cap = cv2.VideoCapture(self.adresse[i])
            j = 0
@@ -138,18 +163,27 @@ class Fenetre(QtWidgets.QDialog):
            r = cv2.selectROI(frame, False)
            imCrop = frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
            cropped = cv2.resize(imCrop, (128, 128))
-           cv2.imwrite(Dossier + '/' + str(DerniereUtilisation+i) + "_" + "0" + ".jpg", cropped)
+           if (DerniereUtilisation+i<10):
+               cv2.imwrite(Dossier + '/00' + str(DerniereUtilisation+i) + "_" + "0" + ".jpg", cropped)
+           if(DerniereUtilisation+i>9 and DerniereUtilisation+i<100):
+               cv2.imwrite(Dossier + '/0' + str(DerniereUtilisation + i) + "_" + "0" + ".jpg", cropped)
+           if(DerniereUtilisation+i>99):
+               cv2.imwrite(Dossier + '/' + str(DerniereUtilisation + i) + "_" + "0" + ".jpg", cropped)
            j = j + 1
            while (j < int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
                ret, frame = cap.read();
                if ret:
                    imCrop = frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
                    cropped = cv2.resize(imCrop, (128, 128))
-                   cv2.imwrite(Dossier+'/'+str(DerniereUtilisation+i)+"_"+str(j)+".jpg",cropped)
+                   if (DerniereUtilisation + i < 10):
+                       cv2.imwrite(Dossier + '/00' + str(DerniereUtilisation + i) + "_" + str(j) + ".jpg", cropped)
+                   if (DerniereUtilisation + i > 9 and DerniereUtilisation + i < 100):
+                       cv2.imwrite(Dossier + '/0' + str(DerniereUtilisation + i) + "_" + str(j) + ".jpg", cropped)
+                   if (DerniereUtilisation + i > 99):
+                       cv2.imwrite(Dossier + '/' + str(DerniereUtilisation + i) + "_" + str(j) + ".jpg", cropped)
                j = j + 1
+       self.Compteur.setText("")
        cv2.destroyAllWindows()
-
-
 if __name__ == '__main__':
    # Create the Qt Application
    app = QtWidgets.QApplication(sys.argv)
